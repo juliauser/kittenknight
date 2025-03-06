3751b98b0eeea37e8a83f3f185503e7c687313eb
juliauser <julianogueira814@gmail.com>
2025-03-05 22:00:12 -0300

    final final final :)

--- game.py
+++ game.py
@@ -1,74 +1,60 @@
 from pygame import Rect
-
-# Basta utilizar o comando pgzrun para aproveitar o jogo
-#
-# COMANDOS
-# Setas para direita e esquerda para mover
-# Espaço para pular
-#
-# Objetivo
-# Coletar todas as flores
-
+# Bem-vindo(a) ao mundo de Kitten Knight
 
 # Tamanho da janela
 WIDTH = 500
 HEIGHT = 300
 
 # Variáveis de estado
-game_state = "menu" # Pode ser "menu", "game", "game_over" ou "end_game"
-sound_on = True  # Controla o som
+game_state = "menu"     # Pode ser "menu", "game", "game_over" ou "end_game"
+sound_on = True         # Controla o som
 button_width = 120
 button_height = 40
 
-# Botões centralizados na tela
+# Botões do menu
 button_start = Rect((WIDTH - button_width) // 2, 100, button_width, button_height)
 button_sound = Rect((WIDTH - button_width) // 2, 150, button_width, button_height)
 button_exit = Rect((WIDTH - button_width) // 2, 200, button_width, button_height)
 
-# Música de fundo
+# Música
 music.play("background_music")
-music.set_volume(0.1)
+music.set_volume(0.2)
 
-# Plano de fundo para o jogo
+# Background do jogo
 background_img = "background_img"
 
-# Classe base para personagens (Player e Enemy)
+# Classe base para os personagens (Player e Enemy)
 class Character:
     def __init__(self, x, y, sprite_prefix):
-        self.rect = Rect((x, y), (16, 16))
+        self.rect = Rect((x, y), (16, 32))
         self.speed = 1
-        self.status = "idle" # Estado inicial do personagem
-        self.direction = "right"  # Direção inicial do personagem
+        self.status = "idle"        # Estado inicial do personagem
+        self.direction = "right"    # Direção inicial do personagem
         self.gravity = 0.5
         self.velocity_y = 0
         
-        # Imagens para a animação do personagem (idle e moving)
+        # Imagens para a animação do personagem (idle e moving) e controle de animação
         self.idle_right = [Actor(f"{sprite_prefix}_idle1"), Actor(f"{sprite_prefix}_idle2")]
         self.moving_right = [Actor(f"{sprite_prefix}_move1"), Actor(f"{sprite_prefix}_move2")]
 
         self.idle_left = [Actor(f"{sprite_prefix}_idle1_left"), Actor(f"{sprite_prefix}_idle2_left")]
         self.moving_left = [Actor(f"{sprite_prefix}_move1_left"), Actor(f"{sprite_prefix}_move2_left")]
                             
-        # Imagem atual e controle de animação
         self.image_index = 0
         self.current_image = self.idle_right[self.image_index]
-        self.animation_timer = 0  # Timer para controlar o delay entre as trocas de imagem
-        self.animation_delay = 0.2  # Tempo em segundos para trocar de imagem
+        self.animation_timer = 0
+        self.animation_delay = 0.2
 
+    # Reset do status idle e Gravidade
     def move(self):
-        # Resetar status para idle no começo de cada movimento
         self.status = "idle"
-        
-        # Gravidade
         self.velocity_y += self.gravity
         self.rect.y += self.velocity_y
 
     def update_animation(self, dt):
-        # Incrementa o tempo do timer pela quantidade de tempo desde o último frame
-        self.animation_timer += dt
+        self.animation_timer += dt      # Incrementa o tempo pela quantidade de tempo desde o último frame
         if self.animation_timer >= self.animation_delay:
-            # Atualiza a imagem de acordo com o status e direção do personagem
-            if self.status == "idle":
+            if self.status == "idle":   # Atualiza a imagem de acordo com o status e direção do personagem
                 if self.direction == "right":
                     self.current_image = self.idle_right[self.image_index]
                 else:
@@ -80,47 +66,37 @@ class Character:
                 else:
                     self.current_image = self.moving_left[self.image_index % len(self.moving_left)]
                 self.image_index = (self.image_index + 1) % len(self.moving_right)
-
-            # Reinicia o timer após trocar a imagem
-            self.animation_timer = 0
-
-        # Atualiza a posição da imagem
-        self.current_image.pos = self.rect.center
+            self.animation_timer = 0                # Reinicia o timer após trocar a imagem
+        self.current_image.pos = self.rect.center   # Atualiza a posição da imagem
 
     def draw(self):
-        # Desenha a imagem atual na tela
-        self.current_image.draw()
+        self.current_image.draw()   # Desenha a imagem atual na tela
 
-# Classe do Player (herda de Character)
+# Classe do PLAYER (herda de Character). Prefixos e Animações
 class Player(Character):
     def __init__(self, x, y):
-        # Passa um prefixo específico para a animação do jogador
         super().__init__(x, y, "player")
         self.on_ground = True
         self.jump_strength = -10
         self.speed = 3
-
-        # Adiciona animação extra do jogador
         self.idle_right.append(Actor("player_idle3"))
-        
         self.idle_left.append(Actor("player_idle3_left"))
 
-
+    # Movimentação (esquerda e direita) e limitadores
     def move(self):
         super().move()
-        # Movimento para a esquerda e direita
         if keyboard.left:
             self.rect.x -= self.speed
-            if self.rect.left < 0:  # Impede o personagem de ultrapassar a esquerda
+            if self.rect.left < 0:
                 self.rect.left = 0
             self.status = "moving"
-            self.direction = "left"  # Atualiza a direção para esquerda
+            self.direction = "left"
         elif keyboard.right:
             self.rect.x += self.speed
-            if self.rect.right > WIDTH:  # Impede o personagem de ultrapassar a direita
+            if self.rect.right > WIDTH:
                 self.rect.right = WIDTH
             self.status = "moving"
-            self.direction = "right"  # Atualiza a direção para direita
+            self.direction = "right"
 
         # Pulo
         if keyboard.space and self.on_ground:
@@ -129,7 +105,6 @@ class Player(Character):
                 sounds.jump_sound.play()
             self.on_ground = False
             
-
         # Colisão com o chão ou plataformas
         for platform in platforms:
             if self.rect.colliderect(platform.rect) and self.velocity_y > 0:
@@ -138,57 +113,51 @@ class Player(Character):
                 self.on_ground = True
                 break
 
-# Classe do Enemy (herda de Character)
+# Classe do INIMIGO (herda de Character)
 class Enemy(Character):
     def __init__(self, x, y, max_movement):
         super().__init__(x, y, "enemy")
         self.speed = 1  # Velocidade de movimento do inimigo
         self.count = 0
         self.max_movement = max_movement
-    
+        
+    # Movimentação e colisão
     def move(self):
         super().move()
-        # Mover o inimigo de um lado para o outro na plataforma
         if self.direction == "right":
             self.rect.x += self.speed
             self.count += 1
             if self.count >= self.max_movement: 
-                self.direction = "left"  # Muda a direção para esquerda
+                self.direction = "left"
                 self.count -= self.count
         elif self.direction == "left":
             self.rect.x -= self.speed
             self.count += 1
-            if self.count >= self.max_movement:  # Verifica se ultrapassou a borda esquerda
-                self.direction = "right"  # Muda a direção para direita
+            if self.count >= self.max_movement:
+                self.direction = "right"
                 self.count -= self.count
-
-        # Colisão com o chão ou plataformas
         for platform in platforms:
             if self.rect.colliderect(platform.rect) and self.velocity_y > 0:
                 self.rect.bottom = platform.rect.top
                 self.velocity_y = 0
                 break
-
-        self.status = "moving"  # O inimigo está se movendo
-        self.update_animation(0)  # Atualiza a animação do inimigo
+        self.status = "moving"
+        self.update_animation(0)
     
-# Classe para plataformas
 class Platform:
     def __init__(self, x, y):
         self.rect = Rect(x, y, 16, 16)
-        self.image = Actor("platform")  # Carrega a imagem da plataforma
-        self.image.width = 16  # Ajusta a largura da imagem para o tamanho da plataforma
-        self.image.height = 16  # Ajusta a altura da imagem para o tamanho da plataforma
-        self.image.pos = self.rect.center  # Centraliza a imagem na plataforma
-
+        self.image = Actor("platform")
+        self.image.width = 16
+        self.image.height = 16
+        self.image.pos = self.rect.center
     def draw(self):
-        self.image.draw()  # Desenha a plataforma
+        self.image.draw()
 
-
-class Flower:
+class Cherry:
     def __init__(self, x, y):
         self.rect = Rect(x, y, 16, 16)
-        self.image = Actor("flower")
+        self.image = Actor("cherry")
         self.image.width = 16
         self.image.height = 16
         self.image.pos = self.rect.center
@@ -196,19 +165,18 @@ class Flower:
     def draw(self):
         self.image.draw()
 
-# Lista de plataformas , inimigos e flores
+# Lista de plataformas, inimigos e cerejas
 platforms = []
 enemies = []
-flowers = []
+cherrys = []
 
-# Cria o personagem
-player = Player(200, 50)
+player = Player(100, 50)    # Cria o personagem
 
-# Cria o mapa, os inimigos e as flores
+# Cria o mapa, os inimigos e as cerejas
 def create_map():
     x = 0
     while x < WIDTH:
-        platforms.append(Platform(x, HEIGHT-30))
+        platforms.append(Platform(x, HEIGHT-16))
         x += 16
     x = 100
     while x < 200:
@@ -226,11 +194,11 @@ def create_map():
     enemies.append(Enemy(152, 60, 90))
     enemies.append(Enemy(302, 50, 90))
     enemies.append(Enemy(150, 200, 200))
-    flowers.append(Flower(150, 70))
-    flowers.append(Flower(400, 110))
-    flowers.append(Flower(450, 250))
+    cherrys.append(Cherry(150, 70))
+    cherrys.append(Cherry(400, 110))
+    cherrys.append(Cherry(450, 250))
 
-# Função para desenhar a tela
+# Função para desenhar a TELA
 def draw():
     global game_state
     screen.clear()
@@ -243,70 +211,69 @@ def draw():
     elif game_state == "end_game":
         draw_end_game()
 
-# Função para desenhar o menu
+# Função para desenhar o MENU
 def draw_menu():
-    screen.draw.text("Main Menu", center=(WIDTH // 2, 50), fontsize=30)
-    screen.draw.filled_rect(button_start, "blue")
-    screen.draw.text("Start Game", center=button_start.center, fontsize=20, color="white")
+    screen.fill((164,161,208))
+    screen.draw.text("Menu Principal", center=(WIDTH // 2, 50), fontsize=30, color="black")
+    screen.draw.filled_rect(button_start, "pink")
+    screen.draw.text("Iniciar Jogo", center=button_start.center, fontsize=20, color="black")
+    screen.draw.text("Feito com muito carinho <3 por Julia Nogueira Figueiredo", (15, 275), fontsize=15, color="black")
     
-    screen.draw.filled_rect(button_sound, "blue")
-    sound_text = "Sound On" if sound_on else "Sound Off"
-    screen.draw.text(sound_text, center=button_sound.center, fontsize=20, color="white")
+    screen.draw.filled_rect(button_sound, "pink")
+    sound_text = "Musica: ON" if sound_on else "Musica: OFF"
+    screen.draw.text(sound_text, center=button_sound.center, fontsize=20, color="black")
     
-    screen.draw.filled_rect(button_exit, "blue")
-    screen.draw.text("Exit", center=button_exit.center, fontsize=20, color="white")
+    screen.draw.filled_rect(button_exit, "pink")
+    screen.draw.text("Sair", center=button_exit.center, fontsize=20, color="black")
 
-# Função para desenhar o jogo
+# Função para desenhar o JOGO
 def draw_game():
     screen.blit(background_img, (0, 0))
-    
     for platform in platforms:
         platform.draw()
-    
-    player.draw()
-    for flower in flowers:
-        flower.draw()
+        player.draw()
+    for cherry in cherrys:
+        cherry.draw()
     for enemy in enemies:
         enemy.draw()
 
-# Função para desenhar tela de game over
+# Função para desenhar tela de GAME OVER
 def draw_game_over():
-    screen.draw.text("Game Over!", (100, 100), fontsize=40, color="red")
-    screen.draw.text("Pressione Enter para tentar novamente", (100, 150), fontsize=20, color="white")
+    screen.fill((164,161,208))
+    screen.draw.text("Fim de Jogo!", (100, 100), fontsize=40, color="red")
+    screen.draw.text("Aperte 'Enter' para tentar novamente", (100, 150), fontsize=20, color="black")
 
-# Função para desenhar tela de fim de jogo
+# Função para desenhar tela FINAL
 def draw_end_game():
-    screen.draw.text("Fim de jogo!", center=(WIDTH // 2, 50), fontsize=40, color="green")
-    screen.draw.text("Obrigado por jogar!", center=(WIDTH // 2, 150), fontsize=20, color="white")
+    screen.fill((164,161,208))
+    screen.draw.text("Parabens!", (100, 100), fontsize=40, color="black")
+    screen.draw.text("Obrigada por jogar!", (100, 150), fontsize=20, color="black")
+    screen.draw.text("Aperte 'Enter' para retornar ao Menu", (100, 200), fontsize=20, color="black")
 
 # Função para rodar o jogo
 def update(dt):
     global game_state
-    # Verifica se o jogo esta rodando
     if game_state == "game":
-        # Move e anima o jogador
         player.move()
         player.update_animation(dt)
-        # Move e anima os inimigos
         for enemy in enemies:
             enemy.move()
             enemy.update_animation(dt)
-            # Verifica colisão entre jogador e inimigos para determinar derrota
             if player.rect.colliderect(enemy.rect):
                 if sound_on:
                     sounds.game_over_sound.play()
                 game_state = "game_over"
-        # Verifica se o jogador coletou alguma flor
-        for flower in flowers:
-            if player.rect.colliderect(flower.rect):
+        for cherry in cherrys:
+            if player.rect.colliderect(cherry.rect):
                 if sound_on:
-                    sounds.flower_sound.play()
-                flowers.remove(flower)
-        # Verifica se todas as flores já foram coletadas para determinar vitória
-        if len(flowers) == 0:
+                    sounds.cherry_sound.play()
+                cherrys.remove(cherry)
+        if len(cherrys) == 0:
             game_state = "end_game"
-    # Ao perder, verifica se o jogador quer tentar novamente, voltando ao menu
     elif game_state == "game_over":
+        if keyboard.RETURN:
+            game_state ="menu"
+    elif game_state == "end_game":
         if keyboard.RETURN:
             game_state ="menu"
 
@@ -317,15 +284,12 @@ def on_mouse_down(pos):
         sounds.click_sound.play()
     if game_state == "menu":
         if button_start.collidepoint(pos):
-            game_state = "game"  # Iniciar o jogo
-            # Remove todos os componentes pré existentes 
+            game_state = "game" # Iniciar o jogo
             platforms.clear()
             enemies.clear()
-            flowers.clear()
-            # Move o jogador para posição inicial
+            cherrys.clear()
             player.rect.x = 50
             player.rect.y = 50
-            # Cria todos os componentes do jogo
             create_map()
         elif button_sound.collidepoint(pos):
             sound_on = not sound_on
